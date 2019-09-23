48fa10b793263a7b3ecbaea4eb109566b196907b
Yashasvi Goel <31849052+yashasvi-goel@users.noreply.github.com>
2019-09-23 07:56:16 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -1,20 +1,24 @@
 import socket
 import sys
 import json
+import multiprocessing
 from multiprocessing import Manager,Process
+import queue
+import time
 
-def human(Input):
-    print("Human")
-
-def check(Input,dic):
+def check(Input,dic,toBeServed,served,sh):
     print("Checking")
     if(Input in dic.keys()):
         return dic[Input]
     else:
         print("NOPE")
-        return human(Input)
+        sh.send(bytes(Input,"utf-8"))
+        j=sh.recv(4096).decode()
+        print(j)
+        dic[Input]=j
+        return dic[Input]
 
-def response(conn,dic):
+def response(conn,dic,toBeServed,served,sh):
     try:
         while True:
             data=conn.recv(4096)
@@ -23,9 +27,7 @@ def response(conn,dic):
                 conn.close()
                 return
             data=data.decode()
-            print(data)
-            res=check(data,dic)
-            print(res)
+            res=check(data,dic,toBeServed,served,sh)
             conn.send(bytes(res,"utf-8"))
     except:
         pass
@@ -40,15 +42,19 @@ if __name__=="__main__":
     serv.listen(5)
     print(f"listening on port:{port}")
     dic=Manager().dict()
+    toBeServed=Manager().Queue()
+    served=Manager().Queue()
     with open('responses.json','r') as filehandle:
         dic=json.load(filehandle)
-    print(type(dic))
+    sh=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    sh.connect(('',7877))
     while True:
         conn,addr=serv.accept()
-        proc=Process(target=response,args=(conn,dic))#creating process
+        proc=Process(target=response,args=(conn,dic,toBeServed,served,sh))#creating process
         proc.daemon=True
         proc.start()
-#        proc.join()
+        #proc.join()
+
     for p in multiprocessing.active_children():
         p.terminate()
         p.join()
